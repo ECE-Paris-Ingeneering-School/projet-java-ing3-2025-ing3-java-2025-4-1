597df60f95798f59ab740aa35e5a15cc78fd43ed
romeo-rf <63726004+romeo-rf@users.noreply.github.com>
2025-04-24 14:18:17 +0200

    Update RendezVous.java

--- src/Model/RendezVous.java
+++ src/Model/RendezVous.java
@@ -8,17 +8,31 @@ public class RendezVous {
     private int idPatient;
     private int idSpecialiste;
     private int idLieu;
+    private String note;
 
-    public RendezVous(int id, LocalDateTime dateHeure, int idPatient, int idSpecialiste, int idLieu) {
+    public RendezVous(int id, LocalDateTime dateHeure, int idPatient, int idSpecialiste, int idLieu, String note) {
         this.id = id;
         this.dateHeure = dateHeure;
         this.idPatient = idPatient;
         this.idSpecialiste = idSpecialiste;
         this.idLieu = idLieu;
+        this.note = note;
     }
 
     public RendezVous(LocalDateTime dateHeure, int idPatient, int idSpecialiste, int idLieu) {
-        this(0, dateHeure, idPatient, idSpecialiste, idLieu);
+        this(0, dateHeure, idPatient, idSpecialiste, idLieu, null); // null pour la note
+    }
+
+    public RendezVous(int id, LocalDateTime dateHeure, int idPatient, int idSpecialiste, int idLieu) {
+        this(0, dateHeure, idPatient, idSpecialiste, idLieu, null); // null pour la note
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
     }
 
     public int getId() {
